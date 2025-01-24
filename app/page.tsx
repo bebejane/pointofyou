e32d1f6b89db25a855700fe6105b52f918a192ff
Bebe Jane <bjornthief@gmail.com>
2025-01-24 14:13:36 +0100

    hej

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import s from './page.module.scss'
-import Link from "next/link"
-import { apiQuery } from 'next-dato-utils/api';
-import { DraftMode } from 'next-dato-utils/components';
-import { Image } from 'react-datocms';
+import s from "./page.module.scss";
+import Link from "next/link";
+import { apiQuery } from "next-dato-utils/api";
+import { DraftMode } from "next-dato-utils/components";
+import { Image } from "react-datocms";
 
 export default async function Home() {
-
-  return (
-    <>
-      <div className={s.page}>
-        <p>Min homepage</p>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className={s.page}>Point of You</div>
+		</>
+	);
 }
