c271415be7f4b0e8daf231ce1ce14037e93287a9
Bebe Jane <bjornthief@gmail.com>
2025-01-24 14:51:38 +0100

    gql

--- @types/document-modules.d.ts
+++ @types/document-modules.d.ts
@@ -1,4 +1,35 @@
 
+declare module '*/about.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllAbouts: DocumentNode;
+export const About: DocumentNode;
+export const AboutFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/contact.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Contact: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/education.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllEducations: DocumentNode;
+export const Education: DocumentNode;
+export const EducationFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
 declare module '*/ImageFragment.gql' {
   import { DocumentNode } from 'graphql';
   const defaultDocument: DocumentNode;
@@ -53,6 +84,15 @@ declare module '*/SiteFragment.gql' {
 }
     
 
+declare module '*/VideoFragment.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const VideoFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
 declare module '*/global.gql' {
   import { DocumentNode } from 'graphql';
   const defaultDocument: DocumentNode;
@@ -62,10 +102,76 @@ declare module '*/global.gql' {
 }
     
 
+declare module '*/home.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Home: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/news.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllPresses: DocumentNode;
+export const Press: DocumentNode;
+export const PressFragment: DocumentNode;
+export const AllNews: DocumentNode;
+export const News: DocumentNode;
+export const NewsFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/project.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllProjects: DocumentNode;
+export const Project: DocumentNode;
+export const ProjectFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/research.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllResearchs: DocumentNode;
+export const Research: DocumentNode;
+export const ResearchAbout: DocumentNode;
+export const ResearchFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
 declare module '*/seo.gql' {
   import { DocumentNode } from 'graphql';
   const defaultDocument: DocumentNode;
   export const Site: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/sound.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const AllSounds: DocumentNode;
+export const Sound: DocumentNode;
+export const SoundFragment: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/support.gql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Support: DocumentNode;
 
   export default defaultDocument;
 }
