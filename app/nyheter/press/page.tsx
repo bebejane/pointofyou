e1ff57c0d403821e70698d1fbc464d1ce4bfcfb2
Bebe Jane <bjornthief@gmail.com>
2025-02-07 12:20:25 +0100

    todos

--- app/nyheter/press/page.tsx
+++ app/nyheter/press/page.tsx
@@ -1,34 +1,25 @@
-import { apiQuery } from "next-dato-utils/api";
-import { AllPressesDocument } from "@/graphql";
-import s from "./page.module.scss";
-import cn from "classnames";
-import Link from "next/link";
-import { Image } from "react-datocms";
-import Content from "@/components/common/Content";
-import FilterBar from "@/components/common/FilterBar";
-import { parseAsString } from "nuqs/server";
-import { DraftMode } from "next-dato-utils/components";
-import Article from "@/components/common/Article";
-import { format } from "date-fns";
+import { apiQuery } from 'next-dato-utils/api';
+import { AllPressesDocument } from '@/graphql';
+import s from './page.module.scss';
+import Link from 'next/link';
+import { Image } from 'react-datocms';
+import Content from '@/components/common/Content';
+import { DraftMode } from 'next-dato-utils/components';
+import Article from '@/components/common/Article';
+import { format } from 'date-fns';
 
-export default async function PressPage({ searchParams }) {
-	const { allPresses, draftUrl } = await apiQuery<AllPressesQuery, AllPressesQueryVariables>(
-		AllPressesDocument,
-		{ all: true }
-	);
+export default async function PressPage() {
+	const { allPresses, draftUrl } = await apiQuery<AllPressesQuery, AllPressesQueryVariables>(AllPressesDocument, { all: true });
 
 	return (
 		<>
-			<Article
-				title='Press'
-				className={s.press}
-			>
+			<Article title='Press' className={s.press}>
 				<ul>
 					{allPresses.map(({ id, title, slug, image, intro, _firstPublishedAt }) => (
 						<li key={id}>
 							<div>
-								<span className='meta'>{format(new Date(_firstPublishedAt), "MM/dd yyyy")}</span>
-								<Link href={`/nyheter/aktuellt/${slug}`}>
+								<span className='meta'>{format(new Date(_firstPublishedAt), 'MM/dd yyyy')}</span>
+								<Link href={`/nyheter/press/${slug}`}>
 									<h2>{title}</h2>
 									<Content content={intro} />
 								</Link>
