8d8086b641d304aab8156a95f0da5b7f56a974d1
Bebe Jane <bjornthief@gmail.com>
2025-02-19 11:19:45 +0100

    rm bubbles

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -1,10 +1,10 @@
-import { Menu } from "@/lib/menu";
-import s from "./Footer.module.scss";
-import Link from "next/link";
-import Bubbles from "@/components/common/Bubbles";
-import { SupportDocument, AllSoundsDocument } from "../../graphql";
-import { apiQuery } from "next-dato-utils/api";
-import SupportBubble from "./SupportBubble";
+import { Menu } from '@/lib/menu';
+import s from './Footer.module.scss';
+import Link from 'next/link';
+import Bubbles from '@/components/common/Bubbles';
+import { SupportDocument, AllSoundsDocument } from '../../graphql';
+import { apiQuery } from 'next-dato-utils/api';
+import SupportBubble from './SupportBubble';
 
 export default async function Footer({ menu }: { menu: Menu }) {
 	const { support } = await apiQuery<SupportQuery, SupportQueryVariables>(SupportDocument);
@@ -15,10 +15,7 @@ export default async function Footer({ menu }: { menu: Menu }) {
 			<footer className={s.footer}>
 				<nav>
 					<figure>
-						<img
-							src='/images/logo-text-white.svg'
-							alt='Point of You logo'
-						/>
+						<img src='/images/logo-text-white.svg' alt='Point of You logo' />
 					</figure>
 					<ul>
 						{menu.map(({ id, title, sub }) => (
@@ -41,16 +38,17 @@ export default async function Footer({ menu }: { menu: Menu }) {
 				<div className={s.copyright}>
 					<span className={s.text}>Copyright Point of You 2024.</span>
 					<span className={s.about}>
-						Ett projekt av <a href="https://www.iffs.se/" target="new">Institutet för Framtidsstudier</a>. &nbsp; &nbsp;
+						Ett projekt av{' '}
+						<a href='https://www.iffs.se/' target='new'>
+							Institutet för Framtidsstudier
+						</a>
+						. &nbsp; &nbsp;
 						<figure>
-							<img
-								src='/images/framtidsstudier.svg'
-								alt='framtidsstudier'
-							/>
+							<img src='/images/framtidsstudier.svg' alt='framtidsstudier' />
 						</figure>
 					</span>
 				</div>
-				<Bubbles sounds={allSounds} />
+				{/* <Bubbles sounds={allSounds} /> */}
 			</footer>
 		</>
 	);
