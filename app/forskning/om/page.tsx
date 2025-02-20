e11bf2d85d87a5ce7905a5a61c1ed9106da54644
Bebe Jane <bjornthief@gmail.com>
2025-02-20 11:51:26 +0100

    fix

--- app/forskning/om/page.tsx
+++ app/forskning/om/page.tsx
@@ -25,7 +25,7 @@ export default async function AboutResearchPage() {
 				content={contentWrapper.content}
 				light={contentWrapper.headerLight}
 				link={{
-					href: '/forskning?filter=all',
+					href: '/forskning/kunskapsbank',
 					text: 'Läs mer i kunskapsbanken',
 				}}
 			/>
